2e5250b5cf63da51cebdc241cf1295c256bd19bb
Sajgon <anton.xdd@gmail.com>
2016-11-25 09:36:19 +0100

    init

---
+++ web/main.js
@@ -0,0 +1,11 @@
+$('.btn-buy').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+$('.contactUsBtn').on('shown.bs.modal', function () {
+  $('#contactusModal').focus()
+})
+
+$('.loginModalBtn').on('shown.bs.modal', function () {
+  $('#loginModal').focus()
+})
